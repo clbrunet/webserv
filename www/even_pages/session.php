3d79f2163a2d2f92133666d351d462b47bfb286d
clbrunet <brunet.clement5@gmail.com>
2021-06-29 16:27:43 +0200

    feat: add www/even_pages/session.php

---
+++ www/even_pages/session.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$_SESSION['var'] = 'value';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Titre de ma page</title>
+	</head>
+	<body>
+		<h1>var_dump($_SESSION)</h1>
+		<pre><?php var_dump($_SESSION); ?></pre>
+		<p>$_SESSION['var'] =  <?php echo $_SESSION['var']; ?> !<br /></p>
+	</body>
+</html>
